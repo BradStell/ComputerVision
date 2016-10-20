c982693cee43d889761a71add04ac1ab41d39a10
Brad <bradstell87@gmail.com>
2016-10-19 22:33:22 -0400

    removing pics

--- assignment3/hw3/build_vocabulary.m
+++ assignment3/hw3/build_vocabulary.m
@@ -5,20 +5,14 @@ class_num = 30;
 img_per_class = 60;
 img_num = class_num .* img_per_class;
 folder_dir = dir(img_path);
-%allFeatures = nan(1, 100);
+all_features = nan([1 64], 'single');
 
-for i = 1:length(folder_dir)-2
+for i = 1:2
     
     img_dir = dir([img_path,folder_dir(1+2).name,'/*.JPG']);
     if isempty(img_dir)
         img_dir = dir([img_path,folder_dir(1+2).name,'/*.BMP']);
     end
-        
-    img = imread([img_path,folder_dir(1+2).name,'/',img_dir(1).name]);
-    gray = rgb2gray(img);
-    points = detectHarrisFeatures(gray);
-    [allFeatures,validPoints] = extractFeatures(gray, points);
-    all_features = allFeatures.Features;
     
     for j = 2:length(img_dir)        
         img = imread([img_path,folder_dir(1+2).name,'/',img_dir(j).name]);
